663fafcf5fad1c69bd041c699faf277b0c293106
Rafiere <rafapgbr@gmail.com>
2023-11-03 23:00:49 -0300

    feat: add initial project files

---
+++ src/main/java/com/piccodev/introductiontospringshell/Application.java
@@ -0,0 +1,29 @@
+package com.piccodev.introductiontospringshell;
+
+import com.piccodev.introductiontospringshell.client.JsonPlaceholderService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	//Estamos criando um Bean para que póssamos utilizar em outro lugar da aplicação.
+
+	@Bean
+	JsonPlaceholderService jsonPlaceholderService(){
+		WebClient client = WebClient.builder()
+				.baseUrl("https://jsonplaceholder.typicode.com").build();
+
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+
+		return factory.createClient(JsonPlaceholderService.class);
+	}
+}
